a30cb88edc09ac20ccab36b84ae30419d316c016
KristaCalleja <kristacm@gmail.com>
2021-04-27 16:44:04 -0300

    Iterate over all li

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 const apiKey = "at_SsKGF3XZ8YMdYdvtD3t0wewKOV6LQ";
+const queriedIpAddress = document.querySelector('.search-input-show');
+// Use querySelectorAll to manipulate all elements
+const generatedResults = document.querySelectorAll(".listed-results li")
+const generatedLocation = document.querySelector('.location-show');
+const generatedTimezone = document.querySelector('.timezone-show');
+const generatedISP = document.querySelector('.isp-show');
 
-searchInput = document.getElementById('search-input');
+// Iterate over all elements
+generatedResults.forEach((result)=>{
+    result.innerText = "abc"
+});
 
-document.querySelector('form').addEventListener('submit', function (e){
-    e.preventDefault();
+
+// Collect queried IP Address
+var searchInput = document.getElementById('search-input');
+
+// 
+const updateIpField = () => {
+    queriedIpAddress.insertAdjacentHTML("beforeend", searchInput.value);
+}
+     
+
+document.querySelector('form').addEventListener('submit', function (event){
+    event.preventDefault();
     console.log(searchInput.value);
 })
+
+const fetchIP = () => {
+    fetch(`https://geo.ipify.org/api/v1?apiKey=at_SsKGF3XZ8YMdYdvtD3t0wewKOV6LQ&ipAddress=8.8.8.8`)
+        .then(response => response.json())
+}
