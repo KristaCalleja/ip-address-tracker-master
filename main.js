0f23cc0bc1a7058baa40759320502aaa2b464b8b
KristaCalleja <kristacm@gmail.com>
2021-04-27 12:29:27 -0300

    Console log test

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 const apiKey = "at_SsKGF3XZ8YMdYdvtD3t0wewKOV6LQ";
+
+searchInput = document.getElementById('search-input');
+
+document.querySelector('form').addEventListener('submit', function (e){
+    e.preventDefault();
+    console.log(searchInput.value);
+})
