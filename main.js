56f5635e4d9d0aa2b5a1fe654c00b2f68d2fb653
KristaCalleja <kristacm@gmail.com>
2021-04-28 13:21:06 -0300

    Add fetch location on event

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
 const apiKey = "at_SsKGF3XZ8YMdYdvtD3t0wewKOV6LQ";
-const queriedIpAddress = document.querySelector('.search-input-show');
-// Use querySelectorAll to manipulate all elements
-const generatedResults = document.querySelectorAll(".listed-results li")
-const generatedLocation = document.querySelector('.location-show');
-const generatedTimezone = document.querySelector('.timezone-show');
-const generatedISP = document.querySelector('.isp-show');
-
-// Iterate over all elements
-generatedResults.forEach((result)=>{
-    result.innerText = "192.212.174.101"
-});
+// Select the inputted ip address
+const searchInput = document.querySelector(".search-input");
+// Select the tag where the resultant data will be returned
+const generatedLocation = document.querySelector(".location-show");
+// Select form to use for the event listener on "click"
+const form = document.querySelector('form');
 
 
-// Collect queried IP Address
-var searchInput = document.getElementById('search-input');
-
-// 
-const updateIpField = () => {
-    queriedIpAddress.insertAdjacentHTML("beforeend", searchInput.value);
-}
-     
-
-document.querySelector('form').addEventListener('submit', function (event){
+// Create function
+const fetchIpAddress = (event) => {
     event.preventDefault();
-    console.log(searchInput.value);
-})
-
-const fetchIP = () => {
-    fetch(`https://geo.ipify.org/api/v1?apiKey=at_SsKGF3XZ8YMdYdvtD3t0wewKOV6LQ&ipAddress=8.8.8.8`)
+    fetch(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=8.8.8.8`)
+        // Transform the response to JSON
         .then(response => response.json())
-}
+        .then((data) => {
+            const country = data.location.country;
+            const region = data.location.region;
+            const city = data.location.city;
+            const location = `${country}, ${region}, ${city}`;
+            console.log(location);
+            generatedLocation.insertAdjacentHTML("beforeend", location);
+        });
+};
+
+
+form.addEventListener('submit', fetchIpAddress);
